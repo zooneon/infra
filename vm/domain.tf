411224646299fe3e0497934b511260ae4ed81191
zooneon <snsxhdz@naver.com>
2023-12-05 01:20:17 +0900

    vm: tab 수정

--- vm/domain.tf
+++ vm/domain.tf
@@ -1,33 +1,33 @@
 resource "libvirt_domain" "node" {
-	count = var.node_count
-	
-	name        = "node${count.index + 1}"
-	description = "provisioned by terraform"
+  count = var.node_count
+  
+  name        = "node${count.index + 1}"
+  description = "provisioned by terraform"
 
-	vcpu   = var.vcpu_count
-	memory = var.memory_size
+  vcpu   = var.vcpu_count
+  memory = var.memory_size
 
-	qemu_agent = true
-	autostart  = true
+  qemu_agent = true
+  autostart  = true
 
-	network_interface {
-		bridge    = var.bridge_name
-	}
+  network_interface {
+    bridge = var.bridge_name
+  }
 
-	disk {
-		volume_id = libvirt_volume.node_volume[count.index].id
-		scsi 	    = true
-	}
+  disk {
+    volume_id = libvirt_volume.node_volume[count.index].id
+    scsi 	    = true
+  }
 
-	console {
-		type 		    = "pty"
-		target_type = "serial"
-		target_port = 0
-	}
+  console {
+    type 		    = "pty"
+    target_type = "serial"
+    target_port = 0
+  }
 
-	boot_device {
-		dev = [ "hd", "network"]
-	}
+  boot_device {
+    dev = [ "hd", "network"]
+  }
 
-	cloudinit = libvirt_cloudinit_disk.common_init[count.index].id
+  cloudinit = libvirt_cloudinit_disk.common_init[count.index].id
 }
